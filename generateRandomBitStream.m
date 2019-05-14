d5716dd51b3ab43e1327210eabfc5e6c865767f3
muhammad20 <muhammadahmad201196@hotmail.com>
2019-05-14 04:23:17 +0200

    modified generate rand bit stream

--- generateRandomBitStream.m
+++ generateRandomBitStream.m
@@ -1,3 +1,15 @@
 function x = generateRandomBitStream(size)
 
-x = round(rand(1,size));
+randBits = round(rand(1,size));
+%Tb = 40
+x = zeros(1, length(randBits) * 40);
+k = 1;
+for i = 1 : length(randBits)
+    if(randBits(i) == 0)
+        amplitude = -1;
+    else 
+        amplitude = 1;
+    end
+    x(k : k + 40) = amplitude;
+    k = 40 * i + 1;
+end
